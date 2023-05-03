92b7b41c2323d7b6b1c00f6ef5b1453b365553ed
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-03 18:33:48 +0300

    finished header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,27 @@
 import './Header.css';
+import logo from './img/logo.png';
+import search from './img/search.png';
 
 function Header() {
     return (
     <header className="header">
-        header
+        <img className='header__logo' src={logo} alt='' />
+        <ul>
+            <li>Home</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+            <li>Become a member</li>
+            <li>About Us</li>
+        </ul>
+        <div className='header__input'>
+            <input type='text' placeholder='Search here...'/>
+            <button>
+                <img src={search} alt='' />
+            </button>
+        </div>
+        <div className='header__account'>
+            <p>Login / Sign Up</p>
+        </div>
     </header>
     )
 }
