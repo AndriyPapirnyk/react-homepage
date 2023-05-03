c9a781507a941c5ad6648ecc98ba3351f31a04e7
AndriyPapirnyk <papirnykandriy@gmail.com>
2023-05-03 19:08:50 +0300

    finished footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,22 @@
-import './Footer.css'
+import './Footer.css';
+import p1 from './img/Group-1.png'
+import p2 from './img/Group-2.png'
+import p3 from './img/Group-3.png'
+import p4 from './img/Group-4.png'
+import p5 from './img/Group-5.png'
+import p6 from './img/Group.png'
+
+
 
 function Footer() {
     return (
         <footer className='footer'>
-            footer
+            <img src={p5} alt='' />
+            <img src={p4} alt='' />
+            <img src={p3} alt='' />
+            <img src={p2} alt='' />
+            <img src={p1} alt='' />
+            <img src={p6} alt='' />
         </footer>
     )
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,22 @@
-import './Home.css'
+import './Home.css';
+import mainImg from './img/main.png';
+import play from './img/play.png';
 
 function Home() {
     return (
         <div className='home'>
-            home
+            <div className='home__text'>
+                <h1>Find out talented <br></br> <span>freelancer</span> with <br></br> better review faster.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <br></br> incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <br></br> quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                <div>
+                    <button>Discover Now</button>
+                    <button>
+                        <img src={play} alt='' />
+                        <p>Watch Demo</p>
+                    </button>
+                </div>
+            </div>
+            <img className='home__main' src={mainImg} alt='' />
         </div>
     )
 }
